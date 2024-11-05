ea29c10e75ed607e158558b31444f6eef17ab05e
MegaDog666 <Moonandre9999@mail.ru>
2024-11-05 15:04:21 +0300

    Lesson 55 (Hibernate)
    Ну вроде все по проще становится, впринципе создание сущностей не состовляет проблем, тока этот кеш покоя мне не оставляет, а так все более менее нормально, думаю со временем разберусь

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,10 @@
 package org.example;
 
-import org.example.model.Item;
+import org.example.model.Passport;
 import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Hello world!
@@ -18,7 +14,7 @@ public class App
 {
     public static void main( String[] args ) {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class)
-                .addAnnotatedClass(Item.class);
+                .addAnnotatedClass(Passport.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
@@ -26,13 +22,8 @@ public class App
         try {
             session.beginTransaction();
 
-            Person person = new Person("Bob", 30);
-
-            person.addItem(new Item("Item 1"));
-            person.addItem(new Item("Item 2"));
-            person.addItem(new Item("Item 3"));
-
-            session.save(person);
+            Person person = session.get(Person.class, 1);
+            session.remove(person);
 
             session.getTransaction().commit();
         } finally {
