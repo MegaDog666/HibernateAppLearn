2cd9b805bbbcbfba463d54d0315f0729ee5648ad
MegaDog666 <Moonandre9999@mail.ru>
2024-11-06 23:58:06 +0300

    Lesson 57 (Hibernate)
    Ох уж эта база данных, скажу так, я начал обучаться по другому, импакт нереальный, начал лучше понимать, но обучение начинает чуток задерживаться, кароче более дольше но зато лучше, думаю на качество надо делать а не на скорость, в целом этот урок мне понятен, чем отличается LAZY от EAGLE подгрузки
    Скоро там будет самостоятельное задание, думаю я щас скоро буду пересматривать спринг, а еще мб читать книжку, но там все на уровне JDBC скорее всего Template, что в моем случае, беее
    Поэтому, буду пересматривать, а что еще делать мне остается, сам летел быстрее паровоза, сейчас плата за скорость

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,12 @@
 package org.example;
 
-import org.example.model.Actor;
-import org.example.model.Movie;
+import org.example.model.Item;
+import org.example.model.Person;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -17,23 +16,45 @@ import java.util.List;
 public class App 
 {
     public static void main( String[] args ) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Actor.class).addAnnotatedClass(Movie.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
 
         try (sessionFactory) {
             Session session = sessionFactory.getCurrentSession();
-            session.beginTransaction();
+                session.beginTransaction();
 
-            Actor actor = session.get(Actor.class, 2);
-            System.out.println(actor.getMovies());
+            Person person = session.get(Person.class, 1);
+            System.out.println("Получили человека");
 
-            Movie movieToRemove = actor.getMovies().get(0);
-            
-            actor.getMovies().remove(0);
-            movieToRemove.getActors().remove(actor);
+//            Hibernate.initialize(person.getItems()); // Подгружаем ленивые сущности
 
-            session.getTransaction().commit();
+                session.getTransaction().commit();
+
+            System.out.println("Сессия закончилась");
+
+            // Открываем сессию и транкзацию еще раз (Можно сделать в любом другом месте в коде)
+            session = sessionFactory.getCurrentSession();
+                session.beginTransaction();
+
+            System.out.println("Внутри второй транзакции");
+
+//            person = session.merge(person); Если через HQL то можно не делать merge
+
+//            Hibernate.initialize(person.getItems()); // Подгружаем ленивые сущности
+
+            // Так тоже можно, но это больше код, поэтому лучше как сверху, просто тут HQL код
+            List<Item> items = session.createQuery("select i from Item i where i.owner.id = :person_id", Item.class)
+                            .setParameter("person_id", person.getId()).getResultList();
+
+            System.out.println(items);
+
+                session.getTransaction().commit();
+
+            System.out.println("Вне второй сессии");
+
+            // Это работает так как связанные товары были подгружены
+//            System.out.println(person.getItems());
         }
 
     }
